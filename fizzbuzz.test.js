5cff26af48dccda42124c661f2828c6d3c301a34
Jethro Whaley <jethro.whaley@gmail.com>
2019-01-16 11:45:14 +0000

    added a bunch of tests to check multiples, realised I didn't follow the simplest TDD process and jumped straight to multiples

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -15,3 +15,27 @@ test('returns Fizzbuzz instead of 15', () => {
 test('returns number if not a multiple of 3 or 5', () => {
   expect(fizzbuzz(4)).toBe(4);
 });
+
+test('returns fizz instead of multiples of 3', () => {
+  expect(fizzbuzz(9)).toBe('Fizz');
+});
+
+test('returns fizz instead of multiples of 123', () => {
+  expect(fizzbuzz(123)).toBe('Fizz');
+});
+
+test('returns buzz instead of multiples of 55', () => {
+  expect(fizzbuzz(55)).toBe('Buzz');
+});
+
+test('returns buzz instead of multiples of 1370', () => {
+  expect(fizzbuzz(1370)).toBe('Buzz');
+});
+
+test('returns Fizzbuzz instead of 60', () => {
+  expect(fizzbuzz(60)).toBe('Fizzbuzz');
+});
+
+test('returns Fizzbuzz instead of 135', () => {
+  expect(fizzbuzz(135)).toBe('Fizzbuzz');
+});
